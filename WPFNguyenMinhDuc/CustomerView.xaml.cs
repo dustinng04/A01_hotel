91b177a3ac0def7f7fe809e502bfe708cd62e30a
dustinng04 <minhduc27025@gmail.com>
2024-06-11 23:08:06 +0700

    Add project files.

---
+++ WPFNguyenMinhDuc/CustomerView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ModelAndDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFNguyenMinhDuc
+{
+    /// <summary>
+    /// Interaction logic for CustomerView.xaml
+    /// </summary>
+    public partial class CustomerView : Window
+    {
+
+        private readonly Customer _customer;
+
+        public CustomerView(Customer customer)
+        {
+            InitializeComponent();
+            _customer = customer;
+        }
+    }
+}
